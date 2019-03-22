77bb6176042fe9fb627121669258f801125f94e9
Pavel Goultiaev <pavel.goultiaev@gmail.com>
2019-03-22 16:53:35 +0100

    rebuild of build step

--- concourse/scripts/build.sh
+++ concourse/scripts/build.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 set -e
-spring-gitrepo/gradlew --no-daemon --build-file spring-gitrepo/build.gradle build -x test
-cp spring-gitrepo/build/libs/*.jar build-output/
+gradle build
--- concourse/scripts/unittest.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-set -e
-cd spring-gitrepo
-./gradlew --no-daemon test
