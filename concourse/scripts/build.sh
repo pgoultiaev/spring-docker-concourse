108c0d7a1ce2ee4dd0a44d508d97e8221d71dd21
Pavel Goultiaev <pavel.goultiaev@gmail.com>
2019-03-22 15:44:40 +0100

    fixed typo

--- concourse/scripts/build.sh
+++ concourse/scripts/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
 spring-gitrepo/gradlew --no-daemon --build-file spring-gitrepo/build.gradle build -x test
-cp spring-boot-concourse/build/libs/*.jar build-output/
+cp spring-gitrepo/build/libs/*.jar build-output/
